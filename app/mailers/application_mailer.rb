7b2139df3bbf9fecdf1cbce5f1d480837a869ef9
sanra9 <sanrra999@gmail.com>
2019-04-30 11:29:04 -0500

    Setup Sendgrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'sanra9@outlook.com'
   layout 'mailer'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'sanra9@outlook.com',
+  :password => 'sendgrid.0159',
+  :domain => 'https://calm-river-94599.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
