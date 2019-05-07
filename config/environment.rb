660b5169c50d380971bd7399b6e234be245b6ac8
sanra9 <sanrra999@gmail.com>
2019-05-07 11:26:05 -0500

    Modify environment4

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,6 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'sanra9@outlook.com',
-  :password => 'sendgrid.0159',
   :domain => 'https://calm-river-94599.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
