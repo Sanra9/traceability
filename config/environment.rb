f853139504d436359625bef206114f2d5282b00b
sanra9 <sanrra999@gmail.com>
2019-05-07 10:06:39 -0500

    Modify environment

--- config/environment.rb
+++ config/environment.rb
@@ -14,5 +14,5 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 }
 echo "export SENDGRID_API_KEY='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'" > sendgrid.env
-echo "sendgrid.env" >> .gitignore
+echo "sendgrid.env"
 source ./sendgrid.env
