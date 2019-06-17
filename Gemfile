2fc620c1e5e617c0a17f54352a65e287ecc73d14
sanra9 <sanrra999@gmail.com>
2019-06-17 17:18:34 -0500

    fix gmail2

--- Gemfile
+++ Gemfile
@@ -15,7 +15,6 @@ gem 'jquery-rails'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
-gem 'sendgrid-ruby'
 gem 'figaro'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :domain => 'https://calm-river-94599.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
